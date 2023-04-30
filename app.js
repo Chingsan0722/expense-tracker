8b4ae530417906d68b77c6a3af21174df5433c2e
Chingsan0722 <rory85722@gmail.com>
2023-04-30 16:05:49 +0800

    initailize project

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
+const session = require('express-session')
+const usePassport = require('./config/passport')
+const flash = require('connect-flash')
+const PORT = process.env.PORT || 3000
+if(process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const routes = require('./routes')
+require('./config/mongoose')
+const app = express()
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname:'.hbs' }))
+app.set('view engine', 'hbs')
+app.use(express.urlencoded({ extended: true }))
+
+app.use(routes)
+
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost${PORT}`)
+})
