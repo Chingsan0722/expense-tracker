49018a33c25131984965d9660abd3b2178ab2c03
Chingsan0722 <rory85722@gmail.com>
2023-05-01 11:48:47 +0800

    feat: set require pages style

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const home = require('./modules/home')
-
+const users = require('./modules/users')
+const newCost = require('./modules/new')
+router.use('/new', newCost)
+router.use('/users', users)
 router.use('/', home)
-
 module.exports = router
