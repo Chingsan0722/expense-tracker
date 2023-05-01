f3e804155ead2e8b4dacae4330712d8c708e404b
Chingsan0722 <rory85722@gmail.com>
2023-05-01 20:26:32 +0800

    feat: separate Schema

--- models/record.js
+++ models/record.js
@@ -9,18 +9,21 @@ const expenseSchema = new Schema ({
     type: String,
     required: true 
   },
-  category: {
-    type: String,
-    required: true
-  },
-// 用string儲存就可以避免掉資料回傳到前端時過多資訊問題
   date: {
-    type: String,
+    type: Date,
     required: true
   },
   amount: {
     type: Number,
     required: true
+  },
+  categoryId: {
+    type: Number,
+    required: true
+  },
+  userId: {
+    type: Number,
+    required: false
   }
 })
 
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const Record = require('../../models/record')
+const now = '2023-11-29T08:00:00.000+08:00'
+function localTime(date) {
+  return moment(date).format('YYYY-MM-DD')
+}
+
+
 const CATEGORY = {
   家居物業: "https://fontawesome.com/icons/home?style=solid",
   交通出行: "https://fontawesome.com/icons/shuttle-van?style=solid",
@@ -12,7 +18,16 @@ const CATEGORY = {
 router.get('/', (req, res) => {
   Record.find()
     .lean()
-    .then(expenseData => res.render('index', { expenseData }))
+    // .then(expenseData => {
+    //   return Promise.all(
+    //     Array.from(
+    //       expenseData.length, (expenseData) => {
+    //         const newDate = localTime(expenseData.date)
+    //         expenseData.date = newDate
+    //       }))
+    //     .then(() => console.log(expenseData))
+    // })
+    .then((expenseData) => res.render('index',{expenseData}))
     .catch(err => console.log(err))
 })
 
