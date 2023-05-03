56977050dcda5161af534256abfdbfda00f4180a
Chingsan0722 <rory85722@gmail.com>
2023-05-03 14:43:36 +0800

    combine npm run seed and exit process

--- models/seeds/categorySeeder.js
+++ models/seeds/categorySeeder.js
@@ -10,35 +10,35 @@ const categories = [
     "icon": 'fa-van-shuttle'
   },
   {
-    "id":3,
+    "id": 3,
     "category": '休閒娛樂',
     "icon": 'fa-face-grin-beam'
   },
-{
-  "id":4,
-  "category": '餐飲食品',
-  "icon": 'fa-utensils'
-},
-{
-  "id":5,
-  "category": '其他',
-  "icon": 'fa-pen'
-},
-{
-  "id":6,
-  "category": '薪資收入',
-  "icon": 'fa-sack-dollar'
-},
-{
-  "id":7,
-  "category": '額外收入',
-  "icon": 'fa-face-surprise'
-},
-{
-  "id":8,
-  "category": '投資收入',
-  "icon": 'fa-arrow-trend-up'
-}]
+  {
+    "id": 4,
+    "category": '餐飲食品',
+    "icon": 'fa-utensils'
+  },
+  {
+    "id": 5,
+    "category": '其他',
+    "icon": 'fa-pen'
+  },
+  {
+    "id": 6,
+    "category": '薪資收入',
+    "icon": 'fa-sack-dollar'
+  },
+  {
+    "id": 7,
+    "category": '額外收入',
+    "icon": 'fa-face-surprise'
+  },
+  {
+    "id": 8,
+    "category": '投資收入',
+    "icon": 'fa-arrow-trend-up'
+  }]
 
 const CATEGORY = {
   家居物業: "https://fontawesome.com/icons/home?style=solid",
@@ -55,10 +55,14 @@ if (process.env.NODE_ENV !== 'production') {
 const Category = require('../category')
 const db = require('../../config/mongoose')
 db.once('open', () => {
-  Category.create(categories)
-  .then(() => {
-  console.log('done.')
-}).catch(err => console.log)
+  return Promise.all([
+    Category.create(categories)
+  ])
+    .then(() => {
+      console.log('done.')
+      process.exit()
+    })
+    .catch(err => console.log(err))
 })
 
 
--- models/seeds/expenseSeeder.js
+++ models/seeds/expenseSeeder.js
@@ -21,28 +21,51 @@ const SEED_USER = [
 ]
 
 db.once('open', () => {
-  Category.find()
-    .then(categories => {
-      const categoriesId = []
-      categories.forEach(category => {
-        categoriesId.push(category._id)
-        categoriesId.push(category.id)
-      })
-      return categoriesId
+  Promise.all(
+    SEED_USER.map((user, index) => {
+      return bcrypt
+        .genSalt(10)
+        .then(salt => bcrypt.hash(user.password, salt))
+        .then(hash =>
+          User.create({
+            name: user.name,
+            email: user.email,
+            password: hash
+          }))
+        .then((user) => {
+          const userId = user._id
+          return Category.find()
+            .then(categories => {
+              const categoriesId = []
+              categories.forEach(category => {
+                categoriesId.push(category._id)
+                categoriesId.push(category.id)
+              })
+              return categoriesId
+            })
+            .then(id => {
+              return Promise.all([
+                Record.create({
+                  amountType: 'expense',
+                  name: 'exp01',
+                  date: '2023-05-02',
+                  amount: 100,
+                  category_id: id[0],
+                  userId: userId,
+                  categoryId: id[1]
+                })
+                .catch(err => console.log(err))
+              ])
+            })
+            .catch(err => console.log(err))
+        })
+        .catch(err => console.log(err))
     })
-    .then(id => {
-      Record.create({
-        amountType: 'expense',
-        name: 'exp01',
-        date: '2023-05-02',
-        amount: 100,
-        category_id: id[0],
-        userId: '6450ae5fa496f11ad4ac15ed',
-        categoryId: id[1]
-      })
+  )
+    .then(() => {
+      console.log('done.')
+      process.exit()
     })
     .catch(err => console.log(err))
 })
-  .then(() => {
-    console.log('done.')
-  })
+
