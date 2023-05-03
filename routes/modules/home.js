196260fe963b51fab9f07709f0ec811232624d10
Chingsan0722 <rory85722@gmail.com>
2023-05-03 11:12:50 +0800

    feat: modify search category function by income and expense

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -37,4 +37,53 @@ router.get('/', (req, res) => {
     .catch(err => console.log(err))
 })
 
+router.get('/:type', (req, res) => {
+  const {type} = req.params
+  let totalAmount = 0
+  if (type === '支出' || '收入') {
+  Category.find()
+    .then(
+      Record.find({ amountType: type })
+        .populate('categoryId')
+        .lean()
+        .then((expenseData) => {
+          expenseData.forEach((data) => {
+            if (data.amountType === '支出') {
+              totalAmount -= data.amount
+            } else {
+              totalAmount += data.amount
+            }
+          })
+          expenseData.forEach((data) => {
+            return data.date = data.date.toISOString().slice(0, 10)
+          })
+          return res.render('index', { expenseData, totalAmount })
+        })
+        .catch(err => console.log(err))
+    )
+    .catch(err => console.log(err))
+      }else{
+    Category.find()
+      .then(
+        Record.find({ amountType: type })
+          .populate('categoryId')
+          .lean()
+          .then((expenseData) => {
+            expenseData.forEach((data) => {
+              if (data.amountType === '支出') {
+                totalAmount -= data.amount
+              } else {
+                totalAmount += data.amount
+              }
+            })
+            expenseData.forEach((data) => {
+              return data.date = data.date.toISOString().slice(0, 10)
+            })
+            return res.render('index', { expenseData, totalAmount })
+          })
+          .catch(err => console.log(err))
+      )
+      .catch(err => console.log(err))
+      }
+})
 module.exports = router
