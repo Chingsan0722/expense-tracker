cbcdfb815e88bcf9fada499a56a6c1d83f6b8ec4
Chingsan0722 <rory85722@gmail.com>
2023-05-16 19:35:34 +0800

    clear unuse code

--- routes/modules/expense.js
+++ routes/modules/expense.js
@@ -103,32 +103,3 @@ router.delete('/:expenseId', (req, res) => {
     })
 })
 module.exports = router
-
-
-// router.post('/', async (req, res) => {
-//   try {
-//     const userId = req.user._id
-//     const categoryId = req.body.filter
-//     if (categoryId === 'all') {
-//       res.redirect('/')
-//     } else {
-//       const details = await Detail.find({ categoryId, userId }).lean()
-//       const categories = await Category.find().lean()
-//       const category = await Category.findOne({ _id: categoryId }).lean()
-//       const mapDetails = details.map((detail) => {
-//         return {
-//           ...detail,
-//           date: detail.date.toLocaleDateString(),
-//           icon: category.icon
-//         }
-//       })
-//       const totalAmount = calculator(mapDetails)
-
-//       res.render('index', { mapDetails, totalAmount, categories, category })
-//     }
-//   } catch (err) {
-//     console.log(err)
-//   }
-// })
-
-// module.exports = router
